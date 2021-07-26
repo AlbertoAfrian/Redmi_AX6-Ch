40d84ca5dafc52d6e1f9cab1a3fef53719588b39
Jor <jorwang@pm.me>
2021-07-26 21:03:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,7 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+git clone https://github.com/fw876/helloworld package/helloworld
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
